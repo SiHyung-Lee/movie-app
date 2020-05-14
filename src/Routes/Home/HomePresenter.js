68ff57554900cb1926d1d739d81c3172a74fd87e
isihyeong <buzzcatfish@gmail.com>
2020-05-15 00:18:38 +0900

    poster 컴포넌트 생성

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div``;
 
@@ -24,54 +25,6 @@ const Title = styled.h2`
     margin-bottom: 30px;
     font-size: 26px;
     font-weight: 700;
-`;
-
-const Poster = styled.div`
-    img {
-        vertical-align: top;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`;
-
-const Desc = styled.div`
-    padding: 26px 15px 12px;
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    strong {
-        width: 100%;
-        display: block;
-        font-size: 1em;
-        font-weight: 700;
-        color: #000;
-        line-height: 1.2;
-    }
-    p {
-        color: rgba(0, 0, 0, 0.6);
-        line-height: 1.5;
-    }
-    span {
-        position: absolute;
-        top: -19px;
-        left: 12px;
-        width: 38px;
-        height: 38px;
-        padding: 2px;
-        border-radius: 50%;
-        background: #081c22;
-        color: #fff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-sizing: border-box;
-        font-size: 14px;
-        &:after {
-            content: '%';
-            font-size: 2px;
-        }
-    }
 `;
 
 const ListMore = styled.button`
@@ -107,21 +60,13 @@ class HomePresenter extends React.Component {
                             <Lists>
                                 {nowPlaying.map((item, idx) => (
                                     <List key={idx}>
-                                        <Link to={`/movie/${item.id}`}>
-                                            <Poster>
-                                                <img
-                                                    src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                                                    alt={item.title}
-                                                />
-                                            </Poster>
-                                            <Desc>
-                                                <strong>{item.title}</strong>
-                                                <p>{item.release_date}</p>
-                                                <span>
-                                                    {item.vote_average * 10}
-                                                </span>
-                                            </Desc>
-                                        </Link>
+                                        <Poster
+                                            id={item.id}
+                                            title={item.title}
+                                            poster={item.poster_path}
+                                            release={item.release_date}
+                                            vote={item.vote_average}
+                                        />
                                     </List>
                                 ))}
                             </Lists>
@@ -132,19 +77,13 @@ class HomePresenter extends React.Component {
                             <Lists>
                                 {upcoming.map((item, idx) => (
                                     <List key={idx}>
-                                        <Poster>
-                                            <img
-                                                src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                                                alt={item.title}
-                                            />
-                                        </Poster>
-                                        <Desc>
-                                            <strong>{item.title}</strong>
-                                            <p>{item.release_date}</p>
-                                            <span>
-                                                {item.vote_average * 10}
-                                            </span>
-                                        </Desc>
+                                        <Poster
+                                            id={item.id}
+                                            title={item.title}
+                                            poster={item.poster_path}
+                                            release={item.release_date}
+                                            vote={item.vote_average}
+                                        />
                                     </List>
                                 ))}
                             </Lists>
@@ -155,19 +94,13 @@ class HomePresenter extends React.Component {
                             <Lists>
                                 {popular.map((item, idx) => (
                                     <List key={idx}>
-                                        <Poster>
-                                            <img
-                                                src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                                                alt={item.title}
-                                            />
-                                        </Poster>
-                                        <Desc>
-                                            <strong>{item.title}</strong>
-                                            <p>{item.release_date}</p>
-                                            <span>
-                                                {item.vote_average * 10}
-                                            </span>
-                                        </Desc>
+                                        <Poster
+                                            id={item.id}
+                                            title={item.title}
+                                            poster={item.poster_path}
+                                            release={item.release_date}
+                                            vote={item.vote_average}
+                                        />
                                     </List>
                                 ))}
                             </Lists>
@@ -178,19 +111,13 @@ class HomePresenter extends React.Component {
                             <Lists>
                                 {topRated.map((item, idx) => (
                                     <List key={idx}>
-                                        <Poster>
-                                            <img
-                                                src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                                                alt={item.title}
-                                            />
-                                        </Poster>
-                                        <Desc>
-                                            <strong>{item.title}</strong>
-                                            <p>{item.release_date}</p>
-                                            <span>
-                                                {item.vote_average * 10}
-                                            </span>
-                                        </Desc>
+                                        <Poster
+                                            id={item.id}
+                                            title={item.title}
+                                            poster={item.poster_path}
+                                            release={item.release_date}
+                                            vote={item.vote_average}
+                                        />
                                     </List>
                                 ))}
                             </Lists>
